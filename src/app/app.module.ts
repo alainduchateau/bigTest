908a19a646256a4e516b0502aaa5d0ed39d3b28c
alainD <alain.duchateau@gmail.com>
2018-05-04 14:50:28 +0200

    car family concept

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { bigDealService } from './services/bigDeal.service';
 import { ListComponent } from './list/list.component';
 import { SingleCarComponent } from './single-car/single-car.component';
+import { CarFamilyComponent } from './car-family/car-family.component';
  
 
 @NgModule({
   declarations: [
     AppComponent,
     ListComponent,
-    SingleCarComponent
+    SingleCarComponent,
+    CarFamilyComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/car-family/car-family.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-car-family',
+  templateUrl: './car-family.component.html',
+  styleUrls: ['./car-family.component.scss']
+})
+export class CarFamilyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
