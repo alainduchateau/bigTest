d2c4d11fb0ea76e907a6474c8428d3d6ae0be11f
alainD <alain.duchateau@gmail.com>
2018-05-08 07:48:39 +0200

    Refining

--- src/app/car-family/car-family.component.ts
+++ src/app/car-family/car-family.component.ts
@@ -9,27 +9,27 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CarFamilyComponent implements OnInit {
 
-  modelFamily:string;
-  modelFamilyList : any;
-  count : number;
-  fuelDiesel : string;
-  fuelGasoline : string;
-  carFuel:string;
-  
-  constructor(private bigDealService:bigDealService, private route: ActivatedRoute) { }
+  modelFamily: string;
+  modelFamilyList: any;
+  count: number;
+  fuelDiesel: string;
+  fuelGasoline: string;
+  carFuel: string;
+
+  constructor(private bigDealServ: bigDealService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    //Retrieve model family for the current url
+    // Retrieve model family for the current url
     this.modelFamily = this.route.snapshot.params['modelFamily'];
 
-    //Download family list for bigDeal service
-    this.modelFamilyList = this.bigDealService.getCarByModel(this.modelFamily); 
+    // Download family list for bigDeal service
+    this.modelFamilyList = this.bigDealServ.getCarByModel(this.modelFamily);
 
-    this.count = this.modelFamilyList.length
+    this.count = this.modelFamilyList.length;
 
-    this.fuelDiesel    = this.modelFamilyList.filter(car  => car.FuelCode == "diesel");
-    this.fuelGasoline  = this.modelFamilyList.filter(car  => car.FuelCode == "gasoline");
-      
+    this.fuelDiesel = this.modelFamilyList.filter(car  => car.FuelCode === 'diesel');
+    this.fuelGasoline  = this.modelFamilyList.filter(car  => car.FuelCode === 'gasoline');
+
   }
 
 }
