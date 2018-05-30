96ddf5e63e511579abca4cabc20b0c59ee346725
alainduchateau <alain.duchateau@gmail.com>
2018-05-30 18:17:42 +0200

    refining loader, percentage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgProgress, NgProgressRef } from '@ngx-progressbar/core';
 import { Subscription } from 'rxjs';
+import { copyConfig } from '@angular/router/src/config';
 
 @Component({
   selector: 'app-root',
@@ -16,6 +17,8 @@ export class AppComponent {
   //Init var used by the template for display the custom loader ( logo fullscreen )
   loading:boolean;
 
+  percent:any;
+
   constructor(private ngProgress: NgProgress) {
     //var retrieving .ref -> reference of NgProgress
     this.progressRef = this.ngProgress.ref();
@@ -25,7 +28,11 @@ export class AppComponent {
       (completed) => {
         //retrieve value of object "completed" with the "active" property 
         this.loading = completed.active;
+        //retrieve percentile of loading in object "completed" 
+        this.percent = completed.value;
     })
+
+    
 
   }
 }
