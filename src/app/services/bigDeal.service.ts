8d8b82338f4107d0b7ac3c3d9c388ac818d195b7
alainD <alain.duchateau@gmail.com>
2018-05-30 11:48:42 +0200

    loading ok

--- src/app/services/bigDeal.service.ts
+++ src/app/services/bigDeal.service.ts
@@ -1,7 +1,6 @@
-import { Subject } from 'rxjs';
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Pipe, PipeTransform } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class bigDealService {
