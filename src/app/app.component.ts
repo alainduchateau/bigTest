ebeaff23bb693799fb486aff0f2cccd3ca3b9437
alainD <alain.duchateau@gmail.com>
2018-05-30 08:51:11 +0200

    loader active listening

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgProgress, NgProgressRef } from '@ngx-progressbar/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  progressRef: NgProgressRef;
+  loadingSubscription: Subscription;
+  loaded:any;
+  constructor(private ngProgress: NgProgress) {
+    this.progressRef = this.ngProgress.ref();
+    this.loadingSubscription = this.progressRef.state$.subscribe(
+      (completed) => {
+        this.loaded = completed.active;
+        console.log(this.loaded);
+    })
+
+  }
 }
 
 
