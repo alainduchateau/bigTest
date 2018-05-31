7fa75e85e0062e4a02e95b4fbfc0d33f5a16de19
alainD <alain.duchateau@gmail.com>
2018-05-31 09:00:07 +0200

    another test for loading animation -> wrong way

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { NgProgress, NgProgressRef } from '@ngx-progressbar/core';
 import { Subscription } from 'rxjs';
 import { copyConfig } from '@angular/router/src/config';
-import { trigger,state,style,transition,animate,keyframes } from '@angular/animations';
+import { trigger,state,style,transition,animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
 
   //Instance of NgProgressRef
@@ -17,7 +18,7 @@ export class AppComponent {
   loadingSubscription: Subscription;
   //Init var used by the template for display the custom loader ( logo fullscreen )
   loading:boolean;
-
+  //Init var to retrive percentage of loading
   percent:any;
 
   constructor(private ngProgress: NgProgress) {
@@ -27,9 +28,10 @@ export class AppComponent {
     this.loadingSubscription = this.progressRef.state$.subscribe(
       //"completed" object return
       (completed) => {
-        //retrieve value of object "completed" with the "active" property 
+        //retrieve value of object "completed" with the "true" property 
         this.loading = completed.active;
-        //retrieve percentile of loading in object "completed" 
+        console.log(this.loading);
+        //retrieve percentage of loading in object "completed" 
         this.percent = completed.value;
     })
 
