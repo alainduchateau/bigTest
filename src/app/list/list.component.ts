b7fd4b9d5f234b9ed631ae4636ce13f652365b75
alainduchateau <alain.duchateau@gmail.com>
2018-06-03 14:01:34 +0200

    filter fuel tye and car type

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -11,6 +11,7 @@ import { Subscription } from 'rxjs';
 export class ListComponent implements OnInit {
 
   listVehicles: any[];
+  referencVehiclesList:any[];
   car: object;
   carFamilies: any[];
   count: number;
@@ -28,6 +29,7 @@ export class ListComponent implements OnInit {
     //Listen the vechicle subjet, trigger the action after datas are loaded
     this.vehiclesSubscription = this.bigDealService.vehiclesSubject.subscribe(
       (vehicles: any[]) => {
+        this.referencVehiclesList = vehicles;
         //Setup the variable to call the result later
         this.listVehicles = vehicles;
         // Group the model by the marketing name
@@ -38,22 +40,63 @@ export class ListComponent implements OnInit {
   }
 
  listyByModels(carList) {
-    carList = carList.filter(car => car.BigDealPrice < 15000);
+    //carList = carList.filter(car => !car.FirstImmatDate);
     this.sortedByFamilies = this.bigDealService.groupBy(carList, car => car.marketingName);
     //carList.filter(car => car.BigDealPrice < 20000);
     this.carFamilies = Array.from(this.sortedByFamilies.keys());
      // Count the total of vehicles in list
      this.count = carList.length;
+     console.log(carList);
+
    
   }
 
   sumByModel(selectedModel){
     return(this.sortedByFamilies.get(selectedModel).length);
   }
-
-  selectCarType(param){
-    console.log("selectCarType "+param);
+/**
+ * Filter the list by fuel type
+ *
+ * @param {string} param => type fuel : diesle, benzine
+ * @memberof ListComponent
+ */
+selectFuelType(param:string){
+    switch(param) {
+      case "diesel":
+          this.listVehicles = this.referencVehiclesList.filter(car => car.FuelCode === "diesel");
+          break;
+      case "gasoline":
+          this.listVehicles = this.referencVehiclesList.filter(car => car.FuelCode === "gasoline");
+          break;
+      default:
+          this.listVehicles = this.referencVehiclesList;
   }
+  this.count = this.listVehicles.length;
+    
+  console.log("selectCarType "+param);
+  }
+
+/**
+ * Filter the list by car type
+ *
+ * @param {string} param => type of car : secondHand, new
+ * @memberof ListComponent
+ */
+selectCarType(param:string){
+  switch(param) {
+    case "secondHand":
+        this.listVehicles = this.referencVehiclesList.filter(car => car.FirstImmatDate);
+        break;
+    case "new":
+        this.listVehicles = this.referencVehiclesList.filter(car => !car.FirstImmatDate);
+        break;
+    default:
+        this.listVehicles = this.referencVehiclesList;
+}
+this.count = this.listVehicles.length;
+  
+console.log("selectCarType "+param);
+}
   
 
 }
