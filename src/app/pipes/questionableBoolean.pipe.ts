753c135e618319ea29b67b83761f3ee0d488b27d
alainD <alain.duchateau@gmail.com>
2018-06-06 07:09:43 +0200

    console.log pipe

--- src/app/pipes/questionableBoolean.pipe.ts
+++ src/app/pipes/questionableBoolean.pipe.ts
@@ -3,6 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'questionableBoolean' })
 export class QuestionableBooleanPipe implements PipeTransform {
     transform(value: boolean): string {
+        console.log(value);
         return value == true ? 'Yes' : 'No'
     }; 
 }
