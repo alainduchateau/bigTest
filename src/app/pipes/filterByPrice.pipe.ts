bf7a9990e37ca5e26566cabe35d90e25ab52834e
alainD <alain.duchateau@gmail.com>
2018-06-12 14:46:31 +0200

    bug fix filterByPrice

--- src/app/pipes/filterByPrice.pipe.ts
+++ src/app/pipes/filterByPrice.pipe.ts
@@ -2,12 +2,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'filterByPrice' })
 export class filterByPricePipe implements PipeTransform {
-  transform(collection: any[], property: number): any[] {
-    
-    //Apply filter collection by property
-    var filteredCollection = collection.filter(car => car.BigDealPrice <= property);
-
-    return filteredCollection;
-
+  transform(collection: any[], property): any[] {
+    if (collection === null ||  property === null) {
+      return collection
+    } else {
+      //Apply filter collection by property
+      var filteredCollection = collection.filter(car => car.BigDealPrice <= +property);
+      return filteredCollection;
+    }
   }
 }
